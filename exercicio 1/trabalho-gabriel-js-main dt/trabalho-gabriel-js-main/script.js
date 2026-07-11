9bb2687c28d7f833476f5aca2179ddf5d40aa4a0
danterlk <da.vidal@escola.pr.gov.br>
2026-07-11 00:12:32 -0300

    Delete exercicio 1 directory

--- exercicio 1/trabalho-gabriel-js-main dt/trabalho-gabriel-js-main/script.js
+++
@@ -1,8 +0,0 @@
-function revelarmensagem() {
-    document.getElementById("mensagem") . innerText = "ola, mundo";
-}
-function mostrarnome() {
-    let nome = document.getElementById("nomeusuario").value;
-
-    document.getElementById("resultadonome").innerText = "bem vindo, " + nome + "!";
-}
